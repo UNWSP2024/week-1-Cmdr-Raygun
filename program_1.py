4185ba205ec4c0ad6a1dae86948cfe8dc0c9722b
Cmdr-Raygun <renttibwerdna@gmail.com>
2024-09-06 23:01:57 -0500

    Code written

--- program_1.py
+++ program_1.py
@@ -1,6 +1,10 @@
 def hello_world():
+    #Title: Hello World
+    # Author: Andrew Bittner
+    # Date: 9/6/2024
+
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
-    print('Hello ...')
+    print('Hello World')
 
 hello_world()
--- program_2.py
+++ program_2.py
@@ -1,6 +1,16 @@
 def proverbs_22_29():
+    # Title: Proverbs 22:29
+    # Author: Andrew Bittner
+    # Date: 9/6/2024
+
     # Write your Proverbs 22:29 code below
-    print('...')
+    print('“Do you see a man skillful in his work?')
+
+    print('He will stand before kings;')
+
+    print('he will not stand before obscure men.”')
+
+    print('(Proverbs 22:29)')
     
 
 proverbs_22_29()
